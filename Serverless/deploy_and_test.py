8ccfc35722b21ea8178aaa813c5de6d8c11a0c6c
Roger Freret <roger.freret@al.infnet.edu.br>
2020-01-30 12:50:29 -0300

    Standard commit

--- Serverless/deploy_and_test.py
+++ Serverless/deploy_and_test.py
@@ -51,7 +51,7 @@ class DeployAndTest:
         print(f'\u001b[33m{log}\x1b[0m')
         # os.system(execute)
         p = subprocess.Popen(execute, close_fds=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(p.communicate())
+        print(p.communicate()[1].decode("utf-8"))
         # while p.poll() is None:
         #     out = p.stdout.read(1)
         #     sys.stdout.write(out.decode("utf-8"))
