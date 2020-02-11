68c783af5dde484ece96bb47a6c308a860452bea
Roger Freret <roger.freret@al.infnet.edu.br>
2020-02-11 00:36:41 -0300

    Test suite v1 complete.

--- Serverless/tests/test_procedure.py
+++ Serverless/tests/test_procedure.py
@@ -71,20 +71,27 @@ class TestProcedure:
             tl.log_alert(f"Successfully parsed test configurations for: {self.required_tests}")
             return True
 
+    def __validate_configs(self):
+        pass
+
     def __run_tests(self):
+
+        # Apply procedure sequentially for every requested test.
         for test in self.required_tests:
 
+            # Prepares data
             self.test_name = test
             self.log_name = self.test_configs.get(test, {}).get('log_name')
             self.function_name = self.test_configs.get(test, {}).get('function_name')
             self.params = self.test_configs.get(test, {}).get('params')
             self.expected = self.test_configs.get(test, {}).get('expected')
-
             self.test_results[self.test_name] = OrderedDict()
             self.timeout_flag = False
 
+            # Informs developer a requested test is initiating.
             tl.log_alert(f"Initiating test '{self.test_name}'...")
 
+            # Launches log monitoring threads, one for each of the observed functions.
             for expected in self.expected:
                 self.test_results[self.test_name][expected.get('function_name')] = OrderedDict()
                 t = threading.Thread(target=self.__monitor_function_log, args=(expected,))
@@ -95,35 +102,43 @@ class TestProcedure:
                     'thread': t
                 })
 
+            # Triggers main function invocation.
             self.__invoke_main_function()
 
+            # Main thread now awaits log extraction, up to a maximum amount of time.
             for thread_entry in self.threads:
                 thread_entry['thread'].join(self.LOG_MONITORING_TIMEOUT)
 
+            # If any log extraction thread is still alive, it's timed out. Aborts it's execution.
             for thread_entry in self.threads:
                 if thread_entry['thread'].is_alive():
                     tl.log_error(f"Function '{thread_entry['function_name']}' log monitoring has timed out. "
                                  f"({self.LOG_MONITORING_TIMEOUT}s)")
                     self.timeout_flag = True
-                    # thread_entry['thread'].join()
-
-            # tl.log(str(self.test_results))
 
             self.__present_test_results()
 
     def __present_test_results(self):
-        tl.log_alert(f"Test '{self.test_name}' completed. Results:")
-        tl.log('.')
+
+        # Prepares data
         test_result = self.test_results.get(self.test_name)
         log_indent_title = ""
         log_indent_test = ""
-        for function_name, results in test_result.items():
-            tl.log(tl.underline(f"{log_indent_title}-> Function '{function_name}'"))
-            log_raw = results.get('log_raw')
 
+        # Informs developer testing has been concluded.
+        tl.log_alert(f"Test '{self.test_name}' completed. Results:")
+        tl.log('.')
+
+        # Checks test results per function.
+        for function_name, results in test_result.items():
+
+            # Logs analyzed function name.
+            tl.log(tl.underline(f"{log_indent_title}-> Function '{function_name}'"))
+
+            # Explodes function execution logs.
             execution_confirmation = results.get('execution_confirmation')
             print_on_screen = self.PRINT_FUNCTION_LOGS_ON_SCREEN if execution_confirmation else True
-
+            log_raw = results.get('log_raw')
             if log_raw:
                 for log_line in log_raw:
                     tl.log(f"{log_indent_test}{log_line}", print_on_screen=print_on_screen)
@@ -131,20 +146,25 @@ class TestProcedure:
             else:
                 tl.log(f"{log_indent_test}No log captured.", print_on_screen=print_on_screen)
 
+            # Exposes criteria by which the log has been selected.
             pick_by = results.get('pick_by')
             if pick_by:
                 tl.log(f"Picked by '{pick_by}'.")
 
+            # Initiates assertion counter.
             assertion_success = 0
 
+            # Shows execution confirmation status
             tl.log(f"{log_indent_test}Execution confirmation. {self.__get_status_string(execution_confirmation)}")
             if execution_confirmation: assertion_success += 1
 
+            # Shows individual function assertions statuses.
             assertion_list = list(results.items())[3:]
             for assertion, status in assertion_list:
                 tl.log(f"{log_indent_test}{assertion} {self.__get_status_string(status)}")
                 if status: assertion_success += 1
 
+            # Shows final function assertion status.
             assertion_no = len(assertion_list)
             if assertion_no+1 == assertion_success:
                 tl.log(tl.paint_status_bg(f"'{function_name}' assertion PASSED ({assertion_success}/{assertion_no + 1} "
@@ -152,49 +172,60 @@ class TestProcedure:
             else:
                 tl.log(tl.paint_status_bg(f"'{function_name}' assertion FAILED ({assertion_success}/{assertion_no + 1} "
                                     f"checks).", False))
-
             tl.log('.')
 
     def __get_status_string(self, status):
+
+        # If status is positive (true), returns standard stylized 'test passed' string.
         if status:
             return tl.paint_status(self.TEST_PASSED_STR, True)
+
+        # If status is negative (false), returns standard stylized 'test failed' string.
         else:
             return tl.paint_status(self.TEST_FAILED_STR, False)
 
     def __monitor_function_log(self, expected):
+
+        # Prepares data
         function_name = expected.get('function_name')
         command = f"sls logs -f {function_name} -t"
         log_open = False
         log_raw = []
+
+        # Runs log monitoring command line via subprocess.Popen
         p = subprocess.Popen(command, bufsize=1, stdin=open(os.devnull), shell=True, stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
+
+        # Iterates on subprocess stdout to capture logs.
         for line in iter(p.stdout.readline, b''):
             line = line.decode("utf-8").replace('\n', '')
 
+            # Initiates a log capture if a 'log start identifier' has been detected.
             if self.LOG_START_IDENTIFIER in line and not log_open:
                 log_open = True
+
+            # Finishes a log capture if a 'lod end identifier' has been detected.
             elif self.LOG_END_IDENTIFIER in line and log_open:
                 log_raw.append(line)
                 log_open = False
                 if self.timeout_flag: return
-                if self.__parse_log(log_raw, self.test_name, expected): return
+                if self.__parse_log(log_raw, expected): return
                 else:
                     log_raw = []
                     continue
 
+            # If a log capture is ongoing, capture this line.
             if log_open:
                 log_raw.append(line)
 
         p.stdout.close()
         p.wait()
 
-    def __parse_log(self, log_raw, test_name, expected) -> bool:
+    def __parse_log(self, log_raw, expected) -> bool:
 
-        # Prepare data
+        # Prepares data
         function_name = expected.get('function_name')
-        execution_confirmation = expected.get('execution_confirmation')
         pick_by = expected.get('pick_by')
-        asserts = expected.get('assert')
         log_str = ''.join(log_raw)
         log_dicts = self.__parse_dicts_from_strings(log_str)
         log_diagnose = OrderedDict({
@@ -203,24 +234,25 @@ class TestProcedure:
             'log_raw': log_raw
         })
 
+        # Informs developer a log has been captured.
         tl.log(f"Log for '{function_name}' function captured.")
 
-        # Declare error if execution confirmation is not found
-        if execution_confirmation not in log_str:
-            self.test_results[test_name][function_name] = log_diagnose
+        # Declares error if execution is not confirmed.
+        if expected.get('execution_confirmation') not in log_str:
+            self.test_results[self.test_name][function_name] = log_diagnose
             return True
         else:
             log_diagnose['execution_confirmation'] = True
 
-        # Check if "pick_by" id is present, discard if not
+        # Checks if "pick_by" id is present, skip log if not.
         if pick_by.strip() and pick_by not in log_str:
-            # self.test_results[test_name] = log_diagnose
             tl.log(f"Skipping log for '{function_name}': 'pickup_by' conditions not met.")
             return False
 
-        # Check asserts
-        for assertion in asserts:
+        # Checks asserts
+        for assertion in expected.get('assert'):
 
+            # If assert was sent in string form, checks existence in string version of log.
             if isinstance(assertion, str):
                 msg = f"Contains '{assertion}'."
                 if assertion in log_str:
@@ -228,6 +260,7 @@ class TestProcedure:
                 else:
                     log_diagnose[msg] = False
 
+            # If assert was sent in dictionary form, searches property and value in dictionaries extracted from log.
             elif isinstance(assertion, dict):
                 props = assertion.keys()
                 msg = "'{}': {} => {}."
@@ -241,21 +274,8 @@ class TestProcedure:
                     elif not prop_value:
                         log_diagnose[msg_not_found.format(prop, assertion[prop])] = False
 
-
-
-                    # prop_found = False
-                    # for log_dict in log_dicts:
-                    #     if prop in log_dict:
-                    #         if log_dict[prop] == assertion[prop]:
-                    #             log_diagnose[msg.format(prop, assertion[prop], log_dict[prop])] = True
-                    #         else:
-                    #             log_diagnose[msg.format(prop, assertion[prop], log_dict[prop])] = False
-                    #         prop_found = True
-                    #         break
-                    # if not prop_found:
-                    #     log_diagnose[msg_not_found.format(prop, assertion[prop])] = False
-
-        self.test_results[test_name][function_name] = log_diagnose
+        # Saves log diagnose to main test results object
+        self.test_results[self.test_name][function_name] = log_diagnose
         return True
 
     def __invoke_main_function(self):
